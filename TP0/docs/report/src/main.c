82d06e0a492797acd06fbb9164ea024fbf0a01b3
Ignacio Santiago Husain <santiago.husain@gmail.com>
2018-09-07 14:18:04 -0300

    Creo estructura básica.

---
+++ TP0/docs/report/src/main.c
@@ -0,0 +1,38 @@
+/* -----------------------------------------------------------
+ @Title:   FIUBA - 66.20 Organización de Computadoras.
+ @Project: TP0 - Infraestructura básica.
+--------------------------------------------------------------
+ @Filename: main.c
+--------------------------------------------------------------
+ @Authors:
+    Álvarez, Natalia Nayla.
+          xxx at xxx dot xxx
+    Husain, Ignacio Santiago.
+          santiago.husain at gmail dot com
+    Verstraeten, Federico.
+          federico.verstraeten at gmail dot com
+
+ @Date:               07-Sep-2018 12:55:41 pm
+ @Last modified by:   santiago
+ @Last modified time: 07-Sep-2018 12:57:03 pm
+
+ @Copyright(C):
+    This file is part of 'TP0 - Infraestructura básica.'.
+    Unauthorized copying or use of this file via any medium
+    is strictly prohibited.
+--------------------------------------------------------------
+
+Check memory leaks with the following command:
+valgrind --tool=memcheck --leak-check=full \
+--show-leak-kinds=all -v ./tp0
+
+----------------------------------------------------------- */
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+  return EXIT_SUCCESS;
+}
