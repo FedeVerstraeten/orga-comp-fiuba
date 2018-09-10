0fef89213c42add56037f86329bf1fd2835908bb
Ignacio Santiago Husain <santiago.husain@gmail.com>
2018-09-10 14:35:55 -0300

    Complete tests suite.

--- TP0/src/runTests.sh
+++ TP0/src/runTests.sh
@@ -16,7 +16,7 @@
 #
 # @Date:               07-Sep-2018 2:12:07 pm
 # @Last modified by:   Ignacio Santiago Husain
-# @Last modified time: 10-Sep-2018 11:41:17 am
+# @Last modified time: 10-Sep-2018 2:34:41 pm
 #
 # @Copyright (C):
 #    This file is part of 'TP0 - Infraestructura básica.'.
@@ -29,7 +29,7 @@
 # ------------------------------------------------------------
 
 # Program name to test.
-EXEC='./tp0'
+PROGRAM_NAME='./tp0'
 
 # Colors to be used.
 RED="\e[31m"
@@ -66,23 +66,30 @@ function error_msg() {
   echo -e "  $RED$1$DEFAULT"
 }
 
+# ------------------------------------------------------------
+# Input parameters tests.
+# ------------------------------------------------------------
+
 # Define the expected outputs of each of the test cases with its associated
 # test functions.
-EXPECTED_OUTPUT_INVALID_INPUT_STREAM=("ERROR: Can't open input stream.")
+EXPECTED_OUTPUT_INEXISTENT_INPUT_STREAM=("ERROR: Can't open input stream.")
 
-function test_parameter_input_stream(){
-  header "TEST: bad 'input' option parameters"
+function test1_parameter_input_inexistent_stream(){
+  header "TEST1: inexistent 'input' stream."
 
-	commands=("-i 1")
+	commands=(
+  "-i 1"
+  "-i hola.bin"
+  )
 
 	for i in "${commands[@]}"
 	do
 
-		msg_testing "$EXEC $i"
+		msg_testing "$PROGRAM_NAME $i"
 
-		PROGRAM_OUTPUT=$($EXEC $i 2>&1)
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
 
-    if [[ "$EXPECTED_OUTPUT_INVALID_INPUT_STREAM" == "$PROGRAM_OUTPUT" ]]; then
+    if [[ "$EXPECTED_OUTPUT_INEXISTENT_INPUT_STREAM" == "$PROGRAM_OUTPUT" ]]; then
       msg_true "$PROGRAM_OUTPUT"
     else
       msg_false "$PROGRAM_OUTPUT"
@@ -91,19 +98,75 @@ function test_parameter_input_stream(){
 	done
 }
 
-EXPECTED_OUTPUT_INVALID_OUTPUT_STREAM=("ERROR: Can't open output stream.")
+EXPECTED_OUTPUT_INPUT_NO_ARGUMENT=("./tp0: option requires an argument -- 'i'")
 
-function test_parameter_output_stream(){
-  header "TEST: bad 'output' option parameters"
+function test11_parameter_input_no_argument(){
+  header "TEST11: no 'input' option parameters."
 
-	commands=("-o .")
+	commands=("-i ")
 
 	for i in "${commands[@]}"
 	do
 
-		msg_testing "$EXEC $i"
+		msg_testing "$PROGRAM_NAME $i"
 
-		PROGRAM_OUTPUT=$($EXEC $i 2>&1)
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
+
+    if [[ "$EXPECTED_OUTPUT_INPUT_NO_ARGUMENT" == "$PROGRAM_OUTPUT" ]]; then
+      msg_true "$PROGRAM_OUTPUT"
+    else
+      msg_false "$PROGRAM_OUTPUT"
+    fi
+
+	done
+}
+
+EXPECTED_OUTPUT_INPUT_INVALID_STREAM=("ERROR: Invalid input stream.")
+
+function test12_parameter_input_invalid_stream(){
+  header "TEST12: invalid 'input' stream."
+
+	commands=(
+  "-i ."
+  "-i .."
+  "-i /"
+  "-i //"
+  )
+
+	for i in "${commands[@]}"
+	do
+
+		msg_testing "$PROGRAM_NAME $i"
+
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
+
+    if [[ "$EXPECTED_OUTPUT_INPUT_INVALID_STREAM" == "$PROGRAM_OUTPUT" ]]; then
+      msg_true "$PROGRAM_OUTPUT"
+    else
+      msg_false "$PROGRAM_OUTPUT"
+    fi
+
+	done
+}
+
+EXPECTED_OUTPUT_INVALID_OUTPUT_STREAM=("ERROR: Invalid output stream.")
+
+function test2_parameter_output_stream(){
+  header "TEST2: invalid 'output' stream."
+
+	commands=(
+  "-i ../tests/test1.bin -o ."
+  "-i ../tests/test1.bin -o .."
+  "-i ../tests/test1.bin -o /"
+  "-i ../tests/test1.bin -o //"
+  )
+
+	for i in "${commands[@]}"
+	do
+
+		msg_testing "$PROGRAM_NAME $i"
+
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
 
     if [[ "$EXPECTED_OUTPUT_INVALID_OUTPUT_STREAM" == "$PROGRAM_OUTPUT" ]]; then
       msg_true "$PROGRAM_OUTPUT"
@@ -114,19 +177,50 @@ function test_parameter_output_stream(){
 	done
 }
 
-EXPECTED_OUTPUT_INVALID_ACTION=("ERROR: Invalid argument.")
+EXPECTED_OUTPUT_OUTPUT_NO_ARGUMENT=("./tp0: option requires an argument -- 'o'")
 
-function test_parameter_action(){
-  header "TEST: bad 'action' option parameters"
+function test21_parameter_output_no_argument(){
+  header "TEST21: no 'output' option parameters."
 
-	commands=("-a bad_action")
+	commands=("-o ")
 
 	for i in "${commands[@]}"
 	do
 
-		msg_testing "$EXEC $i"
+		msg_testing "$PROGRAM_NAME $i"
 
-		PROGRAM_OUTPUT=$($EXEC $i 2>&1)
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
+
+    if [[ "$EXPECTED_OUTPUT_OUTPUT_NO_ARGUMENT" == "$PROGRAM_OUTPUT" ]]; then
+      msg_true "$PROGRAM_OUTPUT"
+    else
+      msg_false "$PROGRAM_OUTPUT"
+    fi
+
+	done
+}
+
+EXPECTED_OUTPUT_INVALID_ACTION=("ERROR: Invalid action argument.")
+
+function test3_parameter_action(){
+  header "TEST3: invalid 'action' parameters."
+
+	commands=(
+  "-a bad_action"
+  "-a 1"
+  "-a ."
+  "-a .."
+  "-a /"
+  "-a //"
+  "-a $"
+  )
+
+	for i in "${commands[@]}"
+	do
+
+		msg_testing "$PROGRAM_NAME $i"
+
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
 
     if [[ "$EXPECTED_OUTPUT_INVALID_ACTION" == "$PROGRAM_OUTPUT" ]]; then
       msg_true "$PROGRAM_OUTPUT"
@@ -137,20 +231,44 @@ function test_parameter_action(){
 	done
 }
 
+EXPECTED_OUTPUT_ACTION_NO_ARGUMENT=("./tp0: option requires an argument -- 'a'")
+
+function test31_parameter_action_no_argument(){
+  header "TEST31: no 'action' option parameters."
+
+	commands=("-a ")
+
+	for i in "${commands[@]}"
+	do
+
+		msg_testing "$PROGRAM_NAME $i"
+
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
+
+    if [[ "$EXPECTED_OUTPUT_ACTION_NO_ARGUMENT" == "$PROGRAM_OUTPUT" ]]; then
+      msg_true "$PROGRAM_OUTPUT"
+    else
+      msg_false "$PROGRAM_OUTPUT"
+    fi
+
+	done
+}
+
 EXPECTED_OUTPUT_VALID_PARAMETERS=()
 
-function test_valid_parameters(){
-  header "TEST: all options with correct parameters."
+function test4_valid_parameters(){
+  header "TEST4: all options with correct parameters."
 
-	commands=(	"-a encode -i ../tests/test1.bin -o ../tests/test1_out.bin"
+	commands=(
+  "-a encode -i ../tests/test1.bin -o ../tests/test1_out.bin"
   "-a decode -i ../tests/test1_out.bin -o ../tests/test2.bin")
 
 	for i in "${commands[@]}"
 	do
 
-		msg_testing "$EXEC $i"
+		msg_testing "$PROGRAM_NAME $i"
 
-		PROGRAM_OUTPUT=$($EXEC $i 2>&1)
+		PROGRAM_OUTPUT=$($PROGRAM_NAME $i 2>&1)
 
 
     if [[ "$EXPECTED_OUTPUT_VALID_PARAMETERS" == "$PROGRAM_OUTPUT" ]]; then
@@ -162,9 +280,101 @@ function test_valid_parameters(){
 	done
 }
 
-# Run the tests.
+# ------------------------------------------------------------
+# Input-output validation tests.
+# ------------------------------------------------------------
+function IO_validation_passed(){
+	echo -e "$GREEN\0PASSED: $DEFAULT $1"
+}
+function IO_validation_not_passed(){
+	echo -e "$RED\0NOT PASSED $DEFAULT $1"
+}
 
-test_parameter_input_stream
-test_parameter_output_stream
-test_parameter_action
-test_valid_parameters
+TESTS_DIR="../tests";
+mkdir $TESTS_DIR;
+
+function test5_IO_validation(){
+  header "TEST5: input-output should be the same."
+
+  n=1;
+  nLimit=$((1024*2));
+
+  while [ $n -le $nLimit ]
+  do
+  	head -c $n </dev/urandom >$TESTS_DIR/in.bin;
+  	$PROGRAM_NAME -a encode -i $TESTS_DIR/in.bin -o $TESTS_DIR/out.b64;
+  	$PROGRAM_NAME -a decode -i $TESTS_DIR/out.b64 -o $TESTS_DIR/out.bin;
+
+  	if diff $TESTS_DIR/in.bin $TESTS_DIR/out.bin; then :;
+  		IO_validation_passed "n = $n";
+  	else
+  		IO_validation_not_passed "n = $n";
+  		break;
+  	fi
+
+  	n=$((n*2));
+
+  	rm -f $TESTS_DIR/in.bin $TESTS_DIR/out.b64 $TESTS_DIR/out.bin
+  done
+}
+
+# ------------------------------------------------------------
+# Encoding-Decoding execution times tests.
+# ------------------------------------------------------------
+function test6_encoding_execution_times(){
+  header "TEST6: encoding execution times."
+
+  n=1;
+  nLimit=$((1024*10000));
+
+  while [ $n -le $nLimit ]
+  do
+  	head -c $n </dev/urandom >$TESTS_DIR/in.bin;
+    ts=$(date +%s%N);
+    $PROGRAM_NAME -a encode -i $TESTS_DIR/in.bin -o $TESTS_DIR/out.b64;
+    tt=$((($(date +%s%N) - $ts)/1000000));
+    printf 'n: %-10d %10s %.2f [ms]\n' "$n" " " "$tt"
+
+  	n=$((n*2));
+
+  	rm -f $TESTS_DIR/in.bin $TESTS_DIR/out.b64 $TESTS_DIR/out.bin
+  done
+}
+
+function test7_decoding_execution_times(){
+  header "TEST7: encoding execution times."
+
+  n=1;
+  nLimit=$((1024*10000));
+
+  while [ $n -le $nLimit ]
+  do
+  	head -c $n </dev/urandom >$TESTS_DIR/in.bin;
+    $PROGRAM_NAME -a encode -i $TESTS_DIR/in.bin -o $TESTS_DIR/out.b64;
+    ts=$(date +%s%N);
+    $PROGRAM_NAME -a decode -i $TESTS_DIR/out.b64 -o $TESTS_DIR/out.bin;
+    tt=$((($(date +%s%N) - $ts)/1000000));
+    printf 'n: %-10d %10s %.2f [ms]\n' "$n" " " "$tt"
+
+  	n=$((n*2));
+
+  	rm -f $TESTS_DIR/in.bin $TESTS_DIR/out.b64 $TESTS_DIR/out.bin
+  done
+}
+
+# ------------------------------------------------------------
+# Run the tests.
+# ------------------------------------------------------------
+test1_parameter_input_inexistent_stream
+test11_parameter_input_no_argument
+test12_parameter_input_invalid_stream
+test2_parameter_output_stream
+test21_parameter_output_no_argument
+test3_parameter_action
+test31_parameter_action_no_argument
+test4_valid_parameters
+test5_IO_validation
+test6_encoding_execution_times
+test7_decoding_execution_times
+
+header "Test suite ended."
